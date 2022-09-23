48d67352ae0111243d513b9b97c7f02fb36a7c56
Maksym <maksymsydorovych@gmail.com>
2022-09-23 18:14:35 +0200

    4.1

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import s from './App.module.css'
+import image from "../../../image/image.jpg"
 import HW1 from '../../../p2-homeworks/h1/HW1'
 import HW2 from "../../../p2-homeworks/h2/HW2";
 import HW3 from "../../../p2-homeworks/h3/HW3";
 import HW4 from "../../../p2-homeworks/h4/HW4";
 function App() {
     return (
-        <div className={s.App}>
+        <div className={s.App} style={{
+            backgroundImage:`url(${image})`,
+            backgroundRepeat: "no-repeat"
+        }}>
             <div>react homeworks:</div>
             <HW1/>
             <HW2/>
